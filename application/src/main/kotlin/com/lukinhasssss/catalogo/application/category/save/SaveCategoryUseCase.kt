354a7f62a53697656f0952d9b89a145bcd0d50e0
Lucas Monteiro <lukasmonteir0@hotmail.com>
2023-05-21 12:10:41 -0300

    feat: add save category usecase

---
+++ application/src/main/kotlin/com/lukinhasssss/catalogo/application/category/save/SaveCategoryUseCase.kt
@@ -0,0 +1,28 @@
+package com.lukinhasssss.catalogo.application.category.save
+
+import com.lukinhasssss.catalogo.application.UseCase
+import com.lukinhasssss.catalogo.domain.category.Category
+import com.lukinhasssss.catalogo.domain.category.CategoryGateway
+import com.lukinhasssss.catalogo.domain.exception.NotificationException
+import com.lukinhasssss.catalogo.domain.validation.Error
+import com.lukinhasssss.catalogo.domain.validation.handler.Notification
+
+class SaveCategoryUseCase(
+    private val categoryGateway: CategoryGateway
+) : UseCase<Category?, Category>() {
+
+    override fun execute(anIn: Category?): Category = with(anIn) {
+        if (this == null) {
+            throw NotificationException.with(anError = Error("A category cannot be null"))
+        }
+
+        val notification = Notification.create()
+        this.validate(notification)
+
+        if (notification.hasError()) {
+            throw NotificationException.with(message = "Invalid category", notification = notification)
+        }
+
+        categoryGateway.save(this)
+    }
+}
