de49cdb73d0ce2efe10dbdb0d8149e4a92d178d7
Lucas Monteiro <lukasmonteir0@hotmail.com>
2024-01-13 23:10:26 -0300

    build: update kotlin, gradle and some dependencies

--- infrastructure/build.gradle.kts
+++ infrastructure/build.gradle.kts
@@ -51,10 +51,11 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("io.micrometer:micrometer-registry-prometheus:1.10.5")
-    implementation("ch.qos.logback:logback-classic:1.4.7")
+    implementation("ch.qos.logback:logback-classic:1.4.14")
+    implementation("ch.qos.logback:logback-core:1.4.14")
     implementation("net.logstash.logback:logstash-logback-encoder:7.3")
 
-    implementation("com.google.guava:guava:31.1-jre")
+    implementation("com.google.guava:guava:32.0.0-android")
 
     testImplementation(project(path = ":domain", configuration = "testClasses"))
 
@@ -69,7 +70,7 @@ dependencies {
     testImplementation("org.testcontainers:testcontainers:${Version.TEST_CONTAINERS}")
     testImplementation("org.testcontainers:junit-jupiter:${Version.TEST_CONTAINERS}")
     testImplementation("com.github.dasniko:testcontainers-keycloak:2.5.0")
-    testImplementation("org.keycloak:keycloak-core:21.1.1")
+    testImplementation("org.keycloak:keycloak-core:23.0.4")
     testImplementation("org.jboss.resteasy:resteasy-core:4.7.9.Final")
     testImplementation("org.jboss.resteasy:resteasy-multipart-provider:4.7.9.Final")
 }
