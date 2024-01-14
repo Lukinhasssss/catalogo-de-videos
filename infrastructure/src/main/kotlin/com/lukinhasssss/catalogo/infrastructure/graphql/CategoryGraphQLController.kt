f2d757cea69fdfce97a6dd008ae50fc88ea32387
Lucas Monteiro <lukasmonteir0@hotmail.com>
2024-01-13 23:34:29 -0300

    feat: implement save category mutation

--- infrastructure/src/main/kotlin/com/lukinhasssss/catalogo/infrastructure/graphql/CategoryGraphQLController.kt
+++ infrastructure/src/main/kotlin/com/lukinhasssss/catalogo/infrastructure/graphql/CategoryGraphQLController.kt
@@ -2,14 +2,19 @@ package com.lukinhasssss.catalogo.infrastructure.graphql
 
 import com.lukinhasssss.catalogo.application.category.list.ListCategoryOutput
 import com.lukinhasssss.catalogo.application.category.list.ListCategoryUseCase
+import com.lukinhasssss.catalogo.application.category.save.SaveCategoryUseCase
+import com.lukinhasssss.catalogo.domain.category.Category
 import com.lukinhasssss.catalogo.domain.category.CategorySearchQuery
+import com.lukinhasssss.catalogo.infrastructure.category.models.CategoryDTO
 import org.springframework.graphql.data.method.annotation.Argument
+import org.springframework.graphql.data.method.annotation.MutationMapping
 import org.springframework.graphql.data.method.annotation.QueryMapping
 import org.springframework.stereotype.Controller
 
 @Controller
 class CategoryGraphQLController(
-    private val listCategoryUseCase: ListCategoryUseCase
+    private val listCategoryUseCase: ListCategoryUseCase,
+    private val saveCategoryUseCase: SaveCategoryUseCase
 ) {
 
     @QueryMapping
@@ -29,5 +34,12 @@ class CategoryGraphQLController(
         )
 
         return listCategoryUseCase.execute(aQuery).data
+    }
+
+    @MutationMapping
+    fun saveCategory(@Argument input: CategoryDTO): Category {
+        val aCategory = input.toCategory()
+        saveCategoryUseCase.execute(aCategory)
+        return aCategory
     }
 }
