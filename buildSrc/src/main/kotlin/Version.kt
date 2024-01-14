3497f29772ede9a0e6a29594db509015074a2336
Lucas Monteiro <lukasmonteir0@hotmail.com>
2024-01-13 23:39:01 -0300

    build: update spring version

--- buildSrc/src/main/kotlin/Version.kt
+++ buildSrc/src/main/kotlin/Version.kt
@@ -5,7 +5,7 @@ object Version {
     const val KTLINT = "11.3.1"
     const val JACOCO = "0.8.10"
     const val SONARQUBE = "4.0.0.2929"
-    const val SPRING_BOOT = "3.1.0"
+    const val SPRING_BOOT = "3.2.1"
     const val SPRING_DEPENDENCY_MANAGEMENT = "1.1.0"
     const val TEST_CONTAINERS = "1.19.3"
     const val VAVR = "0.10.2"
