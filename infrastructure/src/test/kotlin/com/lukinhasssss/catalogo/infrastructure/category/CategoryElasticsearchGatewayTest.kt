6a111c0f64876361291cd47bf96d732e93f3287a
Lucas Monteiro <lukasmonteir0@hotmail.com>
2024-01-20 19:30:59 -0300

    test: add test for method save of category elasticsearch gateway

--- infrastructure/src/test/kotlin/com/lukinhasssss/catalogo/infrastructure/category/CategoryElasticsearchGatewayTest.kt
+++ infrastructure/src/test/kotlin/com/lukinhasssss/catalogo/infrastructure/category/CategoryElasticsearchGatewayTest.kt
@@ -1,7 +1,9 @@
 package com.lukinhasssss.catalogo.infrastructure.category
 
 import com.lukinhasssss.catalogo.AbstractElasticsearchTest
+import com.lukinhasssss.catalogo.domain.Fixture
 import com.lukinhasssss.catalogo.infrastructure.category.persistence.CategoryRepository
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -9,10 +11,36 @@ import org.springframework.beans.factory.annotation.Autowired
 class CategoryElasticsearchGatewayTest : AbstractElasticsearchTest() {
 
     @Autowired
+    private lateinit var categoryGateway: CategoryElasticsearchGateway
+
+    @Autowired
     private lateinit var categoryRepository: CategoryRepository
 
     @Test
     fun testInjection() {
+        assertNotNull(categoryGateway)
         assertNotNull(categoryRepository)
+    }
+
+    @Test
+    fun givenAValidCategory_whenCallsSave_shouldPersistIt() {
+        // given
+        val aulas = Fixture.Categories.aulas
+
+        // when
+        val actualOutput = categoryGateway.save(aulas)
+
+        // then
+        assertEquals(aulas, actualOutput)
+
+        categoryRepository.findById(aulas.id).get().run {
+            assertEquals(aulas.id, id)
+            assertEquals(aulas.name, name)
+            assertEquals(aulas.description, description)
+            assertEquals(aulas.isActive, active)
+            assertEquals(aulas.createdAt, createdAt)
+            assertEquals(aulas.updatedAt, updatedAt)
+            assertEquals(aulas.deletedAt, deletedAt)
+        }
     }
 }
