92e68dc02aeb8106c5850363088b0ea8b5fa5c40
Lucas Monteiro <lukasmonteir0@hotmail.com>
2024-01-27 13:22:12 -0300

    test: add tests for method find by all of category elasticsearch gateway

--- infrastructure/src/test/kotlin/com/lukinhasssss/catalogo/infrastructure/category/CategoryElasticsearchGatewayTest.kt
+++ infrastructure/src/test/kotlin/com/lukinhasssss/catalogo/infrastructure/category/CategoryElasticsearchGatewayTest.kt
@@ -2,6 +2,8 @@ package com.lukinhasssss.catalogo.infrastructure.category
 
 import com.lukinhasssss.catalogo.AbstractElasticsearchTest
 import com.lukinhasssss.catalogo.domain.Fixture
+import com.lukinhasssss.catalogo.domain.category.CategorySearchQuery
+import com.lukinhasssss.catalogo.domain.utils.InstantUtils
 import com.lukinhasssss.catalogo.infrastructure.category.persistence.CategoryDocument
 import com.lukinhasssss.catalogo.infrastructure.category.persistence.CategoryRepository
 import org.junit.jupiter.api.Assertions.assertDoesNotThrow
@@ -11,6 +13,8 @@ import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 import org.springframework.beans.factory.annotation.Autowired
 
 class CategoryElasticsearchGatewayTest : AbstractElasticsearchTest() {
@@ -109,5 +113,180 @@ class CategoryElasticsearchGatewayTest : AbstractElasticsearchTest() {
 
         // then
         assertNull(actualOutput)
+    }
+
+    @Test
+    fun givenEmptyCategories_whenCallsFindAll_shouldReturnEmptyList() {
+        // given
+        val expectedPage = 0
+        val expectedPerPage = 10
+        val expectedTerms = ""
+        val expectedSort = "name"
+        val expectedDirection = "asc"
+        val expectedTotal = 0
+
+        val aQuery = CategorySearchQuery(
+            page = expectedPage,
+            perPage = expectedPerPage,
+            terms = expectedTerms,
+            sort = expectedSort,
+            direction = expectedDirection
+        )
+
+        // when
+        val actualOutput = categoryGateway.findAll(aQuery)
+
+        // then
+        with(actualOutput) {
+            assertEquals(expectedPage, meta.currentPage)
+            assertEquals(expectedPerPage, meta.perPage)
+            assertEquals(expectedTotal.toLong(), meta.total)
+            assertEquals(expectedTotal, data.size)
+        }
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        value = [
+            "aul, 0, 10, 1, 1, Aulas",
+            "liv, 0, 10, 1, 1, Lives"
+        ]
+    )
+    fun givenValidTerm_whenCallsFindAll_shouldReturnElementsFiltered(
+        expectedTerms: String,
+        expectedPage: Int,
+        expectedPerPage: Int,
+        expectedItemsCount: Int,
+        expectedTotal: Long,
+        expectedName: String
+    ) {
+        // given
+        mockCategories()
+
+        val expectedSort = "name"
+        val expectedDirection = "asc"
+
+        val aQuery = CategorySearchQuery(
+            page = expectedPage,
+            perPage = expectedPerPage,
+            terms = expectedTerms,
+            sort = expectedSort,
+            direction = expectedDirection
+        )
+
+        // when
+        val actualOutput = categoryGateway.findAll(aQuery)
+
+        // then
+        with(actualOutput) {
+            assertEquals(expectedPage, meta.currentPage)
+            assertEquals(expectedPerPage, meta.perPage)
+            assertEquals(expectedTotal, meta.total)
+            assertEquals(expectedItemsCount, data.size)
+            assertEquals(expectedName, data[0].name)
+        }
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        value = [
+            "name, asc, 0, 10, 3, 3, Aulas",
+            "name, desc, 0, 10, 3, 3, Talks",
+            "created_at, asc, 0, 10, 3, 3, Aulas",
+            "created_at, desc, 0, 10, 3, 3, Lives"
+        ]
+    )
+    fun givenValidSortAndDirection_whenCallsFindAll_shouldReturnElementsSorted(
+        expectedSort: String,
+        expectedDirection: String,
+        expectedPage: Int,
+        expectedPerPage: Int,
+        expectedItemsCount: Int,
+        expectedTotal: Long,
+        expectedName: String
+    ) {
+        // given
+        mockCategories()
+
+        val expectedTerms = ""
+
+        val aQuery = CategorySearchQuery(
+            page = expectedPage,
+            perPage = expectedPerPage,
+            terms = expectedTerms,
+            sort = expectedSort,
+            direction = expectedDirection
+        )
+
+        // when
+        val actualOutput = categoryGateway.findAll(aQuery)
+
+        // then
+        with(actualOutput) {
+            assertEquals(expectedPage, meta.currentPage)
+            assertEquals(expectedPerPage, meta.perPage)
+            assertEquals(expectedTotal, meta.total)
+            assertEquals(expectedItemsCount, data.size)
+            assertEquals(expectedName, data[0].name)
+        }
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        value = [
+            "0, 1, 1, 3, Aulas",
+            "1, 1, 1, 3, Lives",
+            "2, 1, 1, 3, Talks",
+            "3, 1, 0, 3,"
+        ]
+    )
+    fun givenValidPage_whenCallsFindAll_shouldReturnElementsPaged(
+        expectedPage: Int,
+        expectedPerPage: Int,
+        expectedItemsCount: Int,
+        expectedTotal: Long,
+        expectedName: String?
+    ) {
+        // given
+        mockCategories()
+
+        val expectedTerms = ""
+        val expectedSort = "name"
+        val expectedDirection = "asc"
+
+        val aQuery = CategorySearchQuery(
+            page = expectedPage,
+            perPage = expectedPerPage,
+            terms = expectedTerms,
+            sort = expectedSort,
+            direction = expectedDirection
+        )
+
+        // when
+        val actualOutput = categoryGateway.findAll(aQuery)
+
+        // then
+        with(actualOutput) {
+            assertEquals(expectedPage, meta.currentPage)
+            assertEquals(expectedPerPage, meta.perPage)
+            assertEquals(expectedTotal, meta.total)
+            assertEquals(expectedItemsCount, data.size)
+
+            if (expectedName != null) {
+                assertEquals(expectedName, data[0].name)
+            }
+        }
+    }
+
+    private fun mockCategories() {
+        categoryRepository.save(
+            CategoryDocument.from(Fixture.Categories.aulas).copy(createdAt = InstantUtils.now())
+        )
+        categoryRepository.save(
+            CategoryDocument.from(Fixture.Categories.talks).copy(createdAt = InstantUtils.now())
+        )
+        categoryRepository.save(
+            CategoryDocument.from(Fixture.Categories.lives).copy(createdAt = InstantUtils.now())
+        )
     }
 }
