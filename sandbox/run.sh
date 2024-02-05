b3f458d82357c2dd82c0a99b9ae2cb1bb1dcd35e
Lucas Monteiro <lukasmonteir0@hotmail.com>
2024-02-05 17:50:24 -0300

    feat: add docker-compose for kafka connect

---
+++ sandbox/run.sh
@@ -0,0 +1,18 @@
+printf "Criando as docker networks...\n"
+docker network create elasticsearch-codeflix
+docker network create kafka-catalogo-de-videos
+docker network create admin-do-catalogo-services
+
+# ----------------------------------------------------
+
+printf "Criando os docker volumes...\n"
+docker volume create elasticsearch-codeflix
+docker volume create kafka-catalogo-de-videos
+docker volume create kafka-connect-catalogo-de-videos
+
+# ----------------------------------------------------
+
+printf "Inicializando os container...\n"
+docker compose -f elk/docker-compose.yml up -d
+docker compose -f kafka/docker-compose.yml up -d
+docker compose -f catalogo-services/docker-compose.yml up -d
