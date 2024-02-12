55ecf576cdea3144832b9028aa3562da9457bb73
Lucas Monteiro <lukasmonteir0@hotmail.com>
2024-02-12 01:43:39 -0300

    feat: implement fault tolerance circuit-breaker pattern with resilience4j

--- infrastructure/src/main/kotlin/com/lukinhasssss/catalogo/infrastructure/category/CategoryRestClient.kt
+++ infrastructure/src/main/kotlin/com/lukinhasssss/catalogo/infrastructure/category/CategoryRestClient.kt
@@ -3,6 +3,7 @@ package com.lukinhasssss.catalogo.infrastructure.category
 import com.lukinhasssss.catalogo.infrastructure.category.models.CategoryDTO
 import com.lukinhasssss.catalogo.infrastructure.utils.HttpClient
 import io.github.resilience4j.bulkhead.annotation.Bulkhead
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker
 import io.github.resilience4j.retry.annotation.Retry
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestClient
@@ -18,8 +19,10 @@ class CategoryRestClient(
 
     override fun namespace(): String = NAMESPACE
 
+    // Resilience4j default order: Bulkhead -> TimeLimiter -> RateLimiter -> CircuitBreaker -> Retry
     @Retry(name = NAMESPACE)
     @Bulkhead(name = NAMESPACE)
+    @CircuitBreaker(name = NAMESPACE)
     fun getById(categoryId: String?): CategoryDTO? = doGet(categoryId) {
         restClient.get()
             .uri("/{id}", categoryId)
