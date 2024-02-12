47e086efac7a20f69e59a84ef78b323402290690
Lucas Monteiro <lukasmonteir0@hotmail.com>
2024-02-12 00:45:15 -0300

    build: add resilience4j circuit-breaker dependencie

--- infrastructure/build.gradle.kts
+++ infrastructure/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation("io.github.resilience4j:resilience4j-spring-boot3")
     implementation("io.github.resilience4j:resilience4j-retry")
     implementation("io.github.resilience4j:resilience4j-bulkhead")
+    implementation("io.github.resilience4j:resilience4j-circuitbreaker")
 
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("io.micrometer:micrometer-registry-prometheus:1.10.5")
