5241d1dcb79eb22ded05589272ccd9327989a049
Lucas Monteiro <lukasmonteir0@hotmail.com>
2024-03-08 20:31:48 -0300

    feat: implement client credentials refresh job

--- infrastructure/src/main/kotlin/com/lukinhasssss/catalogo/infrastructure/configuration/WebServerConfig.kt
+++ infrastructure/src/main/kotlin/com/lukinhasssss/catalogo/infrastructure/configuration/WebServerConfig.kt
@@ -2,7 +2,9 @@ package com.lukinhasssss.catalogo.infrastructure.configuration
 
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @Configuration(proxyBeanMethods = false)
 @ComponentScan("com.lukinhasssss.catalogo")
+@EnableScheduling
 class WebServerConfig
