f2321cd26e22d5ace707b6eebbf9bda99887d353
Lucas Monteiro <lukasmonteir0@hotmail.com>
2024-03-30 11:44:37 -0300

    feat: implement method save of cast member elasticsearch gateway

--- infrastructure/src/main/kotlin/com/lukinhasssss/catalogo/infrastructure/castmember/CastMemberElasticsearchGateway.kt
+++ infrastructure/src/main/kotlin/com/lukinhasssss/catalogo/infrastructure/castmember/CastMemberElasticsearchGateway.kt
@@ -4,12 +4,17 @@ import com.lukinhasssss.catalogo.domain.castmember.CastMember
 import com.lukinhasssss.catalogo.domain.castmember.CastMemberGateway
 import com.lukinhasssss.catalogo.domain.castmember.CastMemberSearchQuery
 import com.lukinhasssss.catalogo.domain.pagination.Pagination
+import com.lukinhasssss.catalogo.infrastructure.castmember.persistence.CastMemberDocument
+import com.lukinhasssss.catalogo.infrastructure.castmember.persistence.CastMemberRepository
 import org.springframework.stereotype.Component
 
 @Component
-class CastMemberElasticsearchGateway : CastMemberGateway {
+class CastMemberElasticsearchGateway(
+    private val castMemberRepository: CastMemberRepository
+) : CastMemberGateway {
     override fun save(aMember: CastMember): CastMember {
-        TODO("Not yet implemented")
+        castMemberRepository.save(CastMemberDocument.from(aMember))
+        return aMember
     }
 
     override fun findById(anId: String): CastMember? {
