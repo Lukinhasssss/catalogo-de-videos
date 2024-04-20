aae36409f409f8e55383dd1f8279b4274be62558
Lucas Monteiro <lukasmonteir0@hotmail.com>
2024-04-20 17:58:06 -0300

    feat: implement method save of genre elasticsearch gateway

--- infrastructure/src/main/kotlin/com/lukinhasssss/catalogo/infrastructure/genre/GenreElasticsearchGateway.kt
+++ infrastructure/src/main/kotlin/com/lukinhasssss/catalogo/infrastructure/genre/GenreElasticsearchGateway.kt
@@ -4,13 +4,18 @@ import com.lukinhasssss.catalogo.domain.genre.Genre
 import com.lukinhasssss.catalogo.domain.genre.GenreGateway
 import com.lukinhasssss.catalogo.domain.genre.GenreSearchQuery
 import com.lukinhasssss.catalogo.domain.pagination.Pagination
+import com.lukinhasssss.catalogo.infrastructure.genre.persistence.GenreDocument
+import com.lukinhasssss.catalogo.infrastructure.genre.persistence.GenreRepository
 import org.springframework.stereotype.Component
 
 @Component
-class GenreElasticsearchGateway : GenreGateway {
+class GenreElasticsearchGateway(
+    private val genreRepository: GenreRepository
+) : GenreGateway {
 
     override fun save(genre: Genre): Genre {
-        TODO("Not yet implemented")
+        genreRepository.save(GenreDocument.from(genre))
+        return genre
     }
 
     override fun findById(id: String): Genre? {
