4360f8d5ca76ca9c3b2868a1f8a9532b33e24eae
Lucas Monteiro <lukasmonteir0@hotmail.com>
2024-05-09 21:12:15 -0300

    feat: implement save video usecase

---
+++ application/src/main/kotlin/com/lukinhasssss/catalogo/application/video/save/SaveVideoUseCase.kt
@@ -0,0 +1,68 @@
+package com.lukinhasssss.catalogo.application.video.save
+
+import com.lukinhasssss.catalogo.application.UseCase
+import com.lukinhasssss.catalogo.application.video.save.SaveVideoUseCase.Input
+import com.lukinhasssss.catalogo.application.video.save.SaveVideoUseCase.Output
+import com.lukinhasssss.catalogo.domain.video.Rating
+import com.lukinhasssss.catalogo.domain.video.Video
+import com.lukinhasssss.catalogo.domain.video.VideoGateway
+import java.time.Instant
+import java.time.Year
+
+class SaveVideoUseCase(
+    private val videoGateway: VideoGateway
+) : UseCase<Input, Output>() {
+
+    override fun execute(input: Input): Output = input.run {
+        videoGateway.save(
+            Video.with(
+                id = id,
+                title = title,
+                description = description,
+                launchedAt = launchedAt,
+                duration = duration,
+                rating = rating,
+                opened = opened,
+                published = published,
+                createdAt = createdAt,
+                updatedAt = updatedAt,
+                banner = banner,
+                thumbnail = thumbnail,
+                thumbnailHalf = thumbnailHalf,
+                trailer = trailer,
+                video = video,
+                categories = categories,
+                genres = genres,
+                castMembers = castMembers
+            )
+        ).run { Output(id) }
+    }
+
+    data class Input(
+        val id: String,
+        val title: String,
+        val description: String,
+        val launchedAt: Year,
+        val duration: Double,
+        val rating: Rating,
+
+        val opened: Boolean,
+        var published: Boolean,
+
+        val createdAt: Instant,
+        var updatedAt: Instant,
+
+        var banner: String? = null,
+        var thumbnail: String? = null,
+        var thumbnailHalf: String? = null,
+
+        var trailer: String? = null,
+        var video: String? = null,
+
+        val categories: Set<String> = emptySet(),
+        val genres: Set<String> = emptySet(),
+        val castMembers: Set<String> = emptySet()
+    )
+
+    data class Output(val id: String)
+}
