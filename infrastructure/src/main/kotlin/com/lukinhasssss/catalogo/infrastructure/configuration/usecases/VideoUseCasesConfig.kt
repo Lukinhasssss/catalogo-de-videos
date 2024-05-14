49f19a595726c916c43c269a79fcce7ef2aec4a2
Lucas Monteiro <lukasmonteir0@hotmail.com>
2024-05-13 23:04:05 -0300

    feat: configure video usecase beans

---
+++ infrastructure/src/main/kotlin/com/lukinhasssss/catalogo/infrastructure/configuration/usecases/VideoUseCasesConfig.kt
@@ -0,0 +1,23 @@
+package com.lukinhasssss.catalogo.infrastructure.configuration.usecases
+
+import com.lukinhasssss.catalogo.application.video.delete.DeleteVideoUseCase
+import com.lukinhasssss.catalogo.application.video.list.ListVideoUseCase
+import com.lukinhasssss.catalogo.application.video.save.SaveVideoUseCase
+import com.lukinhasssss.catalogo.domain.video.VideoGateway
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration(proxyBeanMethods = false)
+class VideoUseCasesConfig(
+    private val videoGateway: VideoGateway
+) {
+
+    @Bean
+    fun deleteVideoUseCase() = DeleteVideoUseCase(videoGateway)
+
+    @Bean
+    fun listVideoUseCase() = ListVideoUseCase(videoGateway)
+
+    @Bean
+    fun saveVideoUseCase() = SaveVideoUseCase(videoGateway)
+}
