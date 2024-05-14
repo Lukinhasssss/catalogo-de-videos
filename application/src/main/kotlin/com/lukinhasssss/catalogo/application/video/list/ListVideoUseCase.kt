9db9f0fc5d78a196fe888261b358def893a6234a
Lucas Monteiro <lukasmonteir0@hotmail.com>
2024-05-13 22:52:41 -0300

    feat: implement list video usecase

---
+++ application/src/main/kotlin/com/lukinhasssss/catalogo/application/video/list/ListVideoUseCase.kt
@@ -0,0 +1,68 @@
+package com.lukinhasssss.catalogo.application.video.list
+
+import com.lukinhasssss.catalogo.application.UseCase
+import com.lukinhasssss.catalogo.domain.pagination.Pagination
+import com.lukinhasssss.catalogo.domain.video.Video
+import com.lukinhasssss.catalogo.domain.video.VideoGateway
+import com.lukinhasssss.catalogo.domain.video.VideoSearchQuery
+
+class ListVideoUseCase(
+    private val videoGateway: VideoGateway
+) : UseCase<ListVideoUseCase.Input, Pagination<ListVideoUseCase.Output>>() {
+
+    override fun execute(input: Input): Pagination<Output> = input.run {
+        VideoSearchQuery(
+            page = page,
+            perPage = perPage,
+            terms = terms,
+            sort = sort,
+            direction = direction,
+            rating = rating,
+            launchedAt = launchedAt,
+            categories = categories,
+            castMembers = castMembers,
+            genres = genres
+        ).run {
+            videoGateway.findAll(this).map { Output.from(it) }
+        }
+    }
+
+    data class Input(
+        val page: Int,
+        val perPage: Int,
+        val terms: String,
+        val sort: String,
+        val direction: String,
+        val rating: String,
+        val launchedAt: Int,
+        val categories: Set<String> = setOf(),
+        val castMembers: Set<String> = setOf(),
+        val genres: Set<String> = setOf()
+    )
+
+    data class Output(
+        val id: String,
+        val title: String,
+        val published: Boolean,
+        val launchYear: Int,
+        val rating: String,
+        val categories: Set<String>,
+        val castMembers: Set<String>,
+        val genres: Set<String>
+    ) {
+        companion object {
+            fun from(video: Video) = with(video) {
+                Output(
+                    id = id,
+                    title = title,
+                    published = published,
+                    launchYear = launchedAt.value,
+                    rating = rating.name,
+                    categories = categories,
+                    castMembers = castMembers,
+                    genres = genres
+                )
+            }
+        }
+    }
+}
