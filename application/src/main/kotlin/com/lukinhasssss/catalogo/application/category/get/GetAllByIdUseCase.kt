92753788d6816f053350219923a324acbce41536
Lucas Monteiro <lukasmonteir0@hotmail.com>
2024-05-18 13:28:15 -0300

    feat: implement get all categories by id usecase

---
+++ application/src/main/kotlin/com/lukinhasssss/catalogo/application/category/get/GetAllByIdUseCase.kt
@@ -0,0 +1,35 @@
+package com.lukinhasssss.catalogo.application.category.get
+
+import com.lukinhasssss.catalogo.application.UseCase
+import com.lukinhasssss.catalogo.domain.category.Category
+import com.lukinhasssss.catalogo.domain.category.CategoryGateway
+
+class GetAllByIdUseCase(
+    private val categoryGateway: CategoryGateway
+) : UseCase<GetAllByIdUseCase.Input, List<GetAllByIdUseCase.Output>>() {
+
+    override fun execute(input: Input): List<Output> =
+        if (input.ids.isEmpty()) {
+            emptyList()
+        } else {
+            categoryGateway.findAllById(input.ids).map { Output(it) }
+        }
+
+    data class Input(
+        val ids: List<String> = emptyList()
+    )
+
+    data class Output(
+        val id: String,
+        val name: String,
+        val createdAt: String,
+        val updatedAt: String
+    ) {
+        constructor(category: Category) : this(
+            id = category.id,
+            name = category.name,
+            createdAt = category.createdAt.toString(),
+            updatedAt = category.updatedAt.toString()
+        )
+    }
+}
--- domain/src/main/kotlin/com/lukinhasssss/catalogo/domain/category/CategoryGateway.kt
+++ domain/src/main/kotlin/com/lukinhasssss/catalogo/domain/category/CategoryGateway.kt
@@ -10,5 +10,7 @@ interface CategoryGateway {
 
     fun findAll(aQuery: CategorySearchQuery): Pagination<Category>
 
+    fun findAllById(ids: List<String>): List<Category>
+
     fun deleteById(anID: String)
 }
