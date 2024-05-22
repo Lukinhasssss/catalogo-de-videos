7c971394d4c0668af43dc998ba1826c16e5fe273
Lucas Monteiro <lukasmonteir0@hotmail.com>
2024-05-22 11:57:19 -0300

    test: update tests of video list graphql controller

--- infrastructure/src/test/kotlin/com/lukinhasssss/catalogo/infrastructure/graphql/VideoGraphQLControllerTest.kt
+++ infrastructure/src/test/kotlin/com/lukinhasssss/catalogo/infrastructure/graphql/VideoGraphQLControllerTest.kt
@@ -10,9 +10,11 @@ import com.lukinhasssss.catalogo.domain.pagination.Pagination
 import com.ninjasquad.springmockk.MockkBean
 import io.mockk.every
 import io.mockk.verify
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.graphql.test.tester.GraphQlTester
+import java.time.Instant
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
@@ -37,13 +39,16 @@ class VideoGraphQLControllerTest {
     @Test
     fun givenDefaultArguments_whenCallsListVideos_shouldReturn() {
         // given
+        val cleanCode = Fixture.Videos.cleanCode()
+        val systemDesign = Fixture.Videos.systemDesign()
+
         val categories = listOf(GetAllCategoriesByIdUseCase.Output(Fixture.Categories.lives))
         val castMembers = listOf(GetAllCastMembersByIdUseCase.Output(Fixture.CastMembers.zoro()))
         val genres = listOf(GetAllGenresByIdUseCase.Output(Fixture.Genres.tech()))
 
         val expectedVideos = listOf(
-            ListVideoUseCase.Output.from(Fixture.Videos.cleanCode()),
-            ListVideoUseCase.Output.from(Fixture.Videos.systemDesign())
+            ListVideoUseCase.Output.from(cleanCode),
+            ListVideoUseCase.Output.from(systemDesign)
         )
 
         val expectedPage = 0
@@ -85,16 +90,25 @@ class VideoGraphQLControllerTest {
                 categories {
                   id
                   name
+                  description
                 }
                 castMembersId
                 castMembers {
                   id
                   name
+                  type
+                  createdAt
+                  updatedAt
                 }
                 genresId
                 genres {
                   id
                   name
+                  active
+                  categories
+                  createdAt
+                  updatedAt
+                  deletedAt
                 }
                 createdAt
                 updatedAt
@@ -105,10 +119,98 @@ class VideoGraphQLControllerTest {
         // when
         val res = graphql.document(query).execute()
 
-        val actualVideos = res.path("videos").entityList(ListVideoUseCase.Output::class.java).get()
+        val actualVideos = res.path("videos").entityList(VideoOutput::class.java).get()
 
         // then
-        assertTrue(actualVideos.size == expectedVideos.size && actualVideos.containsAll(expectedVideos))
+        compareVideoOutput(categories, castMembers, genres, expectedVideos[0], actualVideos[0])
+        compareVideoOutput(categories, castMembers, genres, expectedVideos[1], actualVideos[1])
+
+        verify {
+            listVideoUseCase.execute(
+                withArg {
+                    assertEquals(expectedPage, it.page)
+                    assertEquals(expectedPerPage, it.perPage)
+                    assertEquals(expectedSort, it.sort)
+                    assertEquals(expectedDirection, it.direction)
+                    assertEquals(expectedSearch, it.terms)
+                    assertEquals(expectedRating, it.rating)
+                    assertEquals(expectedYearLaunched, it.launchedAt)
+                    assertEquals(expectedCategories, it.categories)
+                    assertEquals(expectedCastMembers, it.castMembers)
+                    assertEquals(expectedGenres, it.genres)
+                }
+            )
+        }
+
+        verify { getAllCategoriesByIdUseCase.execute(withArg { assertEquals(it.ids, cleanCode.categories) }) }
+        verify { getAllCategoriesByIdUseCase.execute(withArg { assertEquals(it.ids, systemDesign.categories) }) }
+
+        verify { getAllCastMembersByIdUseCase.execute(withArg { assertEquals(it.ids, cleanCode.castMembers) }) }
+        verify { getAllCastMembersByIdUseCase.execute(withArg { assertEquals(it.ids, systemDesign.castMembers) }) }
+
+        verify { getAllGenresByIdUseCase.execute(withArg { assertEquals(it.ids, cleanCode.genres) }) }
+        verify { getAllGenresByIdUseCase.execute(withArg { assertEquals(it.ids, systemDesign.genres) }) }
+    }
+
+    @Test
+    fun givenCustomArguments_whenCallsListVideos_shouldReturn() {
+        // given
+        data class VideoIDOuput(val id: String)
+
+        val cleanCode = Fixture.Videos.cleanCode()
+        val systemDesign = Fixture.Videos.systemDesign()
+
+        val expectedVideos = listOf(
+            ListVideoUseCase.Output.from(cleanCode),
+            ListVideoUseCase.Output.from(systemDesign)
+        )
+
+        val expectedVideosId = listOf(
+            VideoIDOuput(cleanCode.id),
+            VideoIDOuput(systemDesign.id)
+        )
+
+        val expectedPage = 2
+        val expectedPerPage = 15
+        val expectedSort = "id"
+        val expectedDirection = "desc"
+        val expectedSearch = "asd"
+        val expectedRating = "L"
+        val expectedYearLaunched = 2024
+        val expectedCategories = setOf("c1")
+        val expectedCastMembers = setOf("cm1")
+        val expectedGenres = setOf("g1")
+
+        every {
+            listVideoUseCase.execute(any())
+        } returns Pagination(expectedPage, expectedPerPage, expectedVideos.size.toLong(), expectedVideos)
+
+        val query = """
+            query AllVideos(${'$'}search: String, ${'$'}page: Int, ${'$'}perPage: Int, ${'$'}sort: String, ${'$'}direction: String, ${'$'}rating: String, ${'$'}yearLaunched: Int, ${'$'}categories: [String], ${'$'}castMembers: [String], ${'$'}genres: [String]) {
+                videos(search: ${'$'}search, page: ${'$'}page, perPage: ${'$'}perPage, sort: ${'$'}sort, direction: ${'$'}direction, rating: ${'$'}rating, yearLaunched: ${'$'}yearLaunched, categories: ${'$'}categories, castMembers: ${'$'}castMembers, genres: ${'$'}genres) {
+                    id
+                }
+            }
+        """.trimIndent()
+
+        // when
+        val res = graphql.document(query)
+            .variable("search", expectedSearch)
+            .variable("page", expectedPage)
+            .variable("perPage", expectedPerPage)
+            .variable("sort", expectedSort)
+            .variable("direction", expectedDirection)
+            .variable("rating", expectedRating)
+            .variable("yearLaunched", expectedYearLaunched)
+            .variable("categories", expectedCategories)
+            .variable("castMembers", expectedCastMembers)
+            .variable("genres", expectedGenres)
+            .execute()
+
+        val actualVideos = res.path("videos").entityList(VideoIDOuput::class.java).get()
+
+        // then
+        assertTrue(actualVideos.size == expectedVideosId.size && actualVideos.containsAll(expectedVideosId))
 
         verify {
             listVideoUseCase.execute(
@@ -128,108 +230,58 @@ class VideoGraphQLControllerTest {
         }
     }
 
-    @Test
-    fun givenCustomArguments_whenCallsListVideos_shouldReturn() {
-        // given
-        val categories = listOf(GetAllCategoriesByIdUseCase.Output(Fixture.Categories.lives))
-        val castMembers = listOf(GetAllCastMembersByIdUseCase.Output(Fixture.CastMembers.zoro()))
-        val genres = listOf(GetAllGenresByIdUseCase.Output(Fixture.Genres.tech()))
+    private data class VideoOutput(
+        val id: String,
+        val title: String,
+        val description: String,
+        val yearLaunched: Int,
+        val rating: String,
+        val duration: Double,
+        val opened: Boolean,
+        val published: Boolean,
+        val banner: String? = null,
+        val thumbnail: String? = null,
+        val thumbnailHalf: String? = null,
+        val trailer: String? = null,
+        val video: String? = null,
+        val categoriesId: Set<String>,
+        val categories: Set<GetAllCategoriesByIdUseCase.Output>,
+        val castMembersId: Set<String>,
+        val castMembers: Set<GetAllCastMembersByIdUseCase.Output>,
+        val genresId: Set<String>,
+        val genres: Set<GetAllGenresByIdUseCase.Output>,
+        val createdAt: Instant,
+        val updatedAt: Instant
+    )
 
-        val expectedVideos = listOf(
-            ListVideoUseCase.Output.from(Fixture.Videos.cleanCode()),
-            ListVideoUseCase.Output.from(Fixture.Videos.systemDesign())
+    private fun compareVideoOutput(
+        expectedCategories: List<GetAllCategoriesByIdUseCase.Output>,
+        expectedCastMembers: List<GetAllCastMembersByIdUseCase.Output>,
+        expectedGenres: List<GetAllGenresByIdUseCase.Output>,
+        expectedVideo: ListVideoUseCase.Output,
+        actualVideo: VideoOutput
+    ) {
+        assertThat(actualVideo)
+            .usingRecursiveComparison().ignoringFields("categories", "castMembers", "genres")
+            .isEqualTo(expectedVideo)
+
+        assertTrue(
+            actualVideo.castMembers.size == expectedCastMembers.size &&
+                actualVideo.castMembers.containsAll(expectedCastMembers)
         )
 
-        val expectedPage = 2
-        val expectedPerPage = 15
-        val expectedSort = "id"
-        val expectedDirection = "desc"
-        val expectedSearch = "asd"
-        val expectedRating = "L"
-        val expectedYearLaunched = 2024
-        val expectedCategories = setOf("c1")
-        val expectedCastMembers = setOf("cm1")
-        val expectedGenres = setOf("g1")
+        assertTrue(
+            (
+                actualVideo.categories.size == expectedCategories.size &&
+                    actualVideo.categories.containsAll(expectedCategories)
+                )
+        )
 
-        every {
-            listVideoUseCase.execute(any())
-        } returns Pagination(expectedPage, expectedPerPage, expectedVideos.size.toLong(), expectedVideos)
-
-        every { getAllCategoriesByIdUseCase.execute(any()) } returns categories
-        every { getAllCastMembersByIdUseCase.execute(any()) } returns castMembers
-        every { getAllGenresByIdUseCase.execute(any()) } returns genres
-
-        val query = """
-            query AllVideos(${'$'}search: String, ${'$'}page: Int, ${'$'}perPage: Int, ${'$'}sort: String, ${'$'}direction: String, ${'$'}rating: String, ${'$'}yearLaunched: Int, ${'$'}categories: [String], ${'$'}castMembers: [String], ${'$'}genres: [String]) {
-                videos(search: ${'$'}search, page: ${'$'}page, perPage: ${'$'}perPage, sort: ${'$'}sort, direction: ${'$'}direction, rating: ${'$'}rating, yearLaunched: ${'$'}yearLaunched, categories: ${'$'}categories, castMembers: ${'$'}castMembers, genres: ${'$'}genres) {
-                    id
-                    title
-                    description
-                    yearLaunched
-                    rating
-                    duration
-                    opened
-                    published
-                    banner
-                    thumbnail
-                    thumbnailHalf
-                    trailer
-                    video
-                    categoriesId
-                    categories {
-                      id
-                      name
-                    }
-                    castMembersId
-                    castMembers {
-                      id
-                      name
-                    }
-                    genresId
-                    genres {
-                      id
-                      name
-                    }
-                    createdAt
-                    updatedAt
-                }
-            }
-        """.trimIndent()
-
-        // when
-        val res = graphql.document(query)
-            .variable("search", expectedSearch)
-            .variable("page", expectedPage)
-            .variable("perPage", expectedPerPage)
-            .variable("sort", expectedSort)
-            .variable("direction", expectedDirection)
-            .variable("rating", expectedRating)
-            .variable("yearLaunched", expectedYearLaunched)
-            .variable("categories", expectedCategories)
-            .variable("castMembers", expectedCastMembers)
-            .variable("genres", expectedGenres)
-            .execute()
-
-        val actualVideos = res.path("videos").entityList(ListVideoUseCase.Output::class.java).get()
-
-        // then
-        assertTrue(actualVideos.size == expectedVideos.size && actualVideos.containsAll(expectedVideos))
-
-        verify {
-            listVideoUseCase.execute(
-                withArg {
-                    assertEquals(expectedPage, it.page)
-                    assertEquals(expectedPerPage, it.perPage)
-                    assertEquals(expectedSort, it.sort)
-                    assertEquals(expectedDirection, it.direction)
-                    assertEquals(expectedSearch, it.terms)
-                    assertEquals(expectedRating, it.rating)
-                    assertEquals(expectedYearLaunched, it.launchedAt)
-                    assertEquals(expectedCategories, it.categories)
-                    assertEquals(expectedCastMembers, it.castMembers)
-                    assertEquals(expectedGenres, it.genres)
-                }
-            )
-        }
+        assertTrue(
+            (
+                actualVideo.genres.size == expectedGenres.size &&
+                    actualVideo.genres.containsAll(expectedGenres)
+                )
+        )
     }
 }
