fc6e384bae18fc03652f59aba0d26fca3daf5da5
Lucas Monteiro <lukasmonteir0@hotmail.com>
2024-05-22 09:44:10 -0300

    feat: implement video gateway in memory

---
+++ infrastructure/src/main/kotlin/com/lukinhasssss/catalogo/infrastructure/video/VideoInMemoryGateway.kt
@@ -0,0 +1,35 @@
+package com.lukinhasssss.catalogo.infrastructure.video
+
+import com.lukinhasssss.catalogo.domain.pagination.Pagination
+import com.lukinhasssss.catalogo.domain.video.Video
+import com.lukinhasssss.catalogo.domain.video.VideoGateway
+import com.lukinhasssss.catalogo.domain.video.VideoSearchQuery
+import org.springframework.stereotype.Component
+
+@Component
+class VideoInMemoryGateway(
+    private val db: MutableMap<String, Video> = mutableMapOf()
+) : VideoGateway {
+
+    override fun save(video: Video): Video {
+        db[video.id] = video
+        return video
+    }
+
+    override fun findById(id: String): Video? {
+        return db[id]
+    }
+
+    override fun findAll(aQuery: VideoSearchQuery): Pagination<Video> = with(aQuery) {
+        Pagination(
+            currentPage = page,
+            perPage = perPage,
+            total = db.size.toLong(),
+            items = db.values.toList()
+        )
+    }
+
+    override fun deleteById(id: String) {
+        db.remove(id)
+    }
+}
