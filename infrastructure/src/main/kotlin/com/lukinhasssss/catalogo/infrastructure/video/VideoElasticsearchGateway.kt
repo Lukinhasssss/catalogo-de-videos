d4699725b07f199f808715131f6be1ca6de08151
Lucas Monteiro <lukasmonteir0@hotmail.com>
2024-05-26 16:01:06 -0300

    feat: apply builder pattern for dynamic query build

--- infrastructure/src/main/kotlin/com/lukinhasssss/catalogo/infrastructure/video/VideoElasticsearchGateway.kt
+++ infrastructure/src/main/kotlin/com/lukinhasssss/catalogo/infrastructure/video/VideoElasticsearchGateway.kt
@@ -1,12 +1,16 @@
 package com.lukinhasssss.catalogo.infrastructure.video
 
-import co.elastic.clients.elasticsearch._types.FieldValue
-import co.elastic.clients.elasticsearch._types.query_dsl.Query
-import co.elastic.clients.elasticsearch._types.query_dsl.QueryBuilders
 import com.lukinhasssss.catalogo.domain.pagination.Pagination
 import com.lukinhasssss.catalogo.domain.video.Video
 import com.lukinhasssss.catalogo.domain.video.VideoGateway
 import com.lukinhasssss.catalogo.domain.video.VideoSearchQuery
+import com.lukinhasssss.catalogo.infrastructure.video.VideoQueryBuilder.Companion.containingCastMembers
+import com.lukinhasssss.catalogo.infrastructure.video.VideoQueryBuilder.Companion.containingCategories
+import com.lukinhasssss.catalogo.infrastructure.video.VideoQueryBuilder.Companion.containingGenres
+import com.lukinhasssss.catalogo.infrastructure.video.VideoQueryBuilder.Companion.launchedAtEquals
+import com.lukinhasssss.catalogo.infrastructure.video.VideoQueryBuilder.Companion.onlyPublished
+import com.lukinhasssss.catalogo.infrastructure.video.VideoQueryBuilder.Companion.ratingEquals
+import com.lukinhasssss.catalogo.infrastructure.video.VideoQueryBuilder.Companion.titleOrDescriptionContaining
 import com.lukinhasssss.catalogo.infrastructure.video.persistence.VideoDocument
 import com.lukinhasssss.catalogo.infrastructure.video.persistence.VideoRepository
 import org.springframework.context.annotation.Profile
@@ -39,36 +43,18 @@ class VideoElasticsearchGateway(
         if (id.isBlank()) null else videoRepository.findById(id).map { it.toVideo() }.orElse(null)
 
     override fun findAll(aQuery: VideoSearchQuery): Pagination<Video> = with(aQuery) {
-        val must = mutableListOf<Query>()
-
-        must.add(QueryBuilders.term { term -> term.field("published").value(true) })
-
-        if (categories.isNotEmpty()) {
-            must.add(QueryBuilders.terms { terms -> terms.field("categories").terms { it.value(fieldValues(categories)) } })
-        }
-
-        if (castMembers.isNotEmpty()) {
-            must.add(QueryBuilders.terms { terms -> terms.field("cast_members").terms { it.value(fieldValues(castMembers)) } })
-        }
-
-        if (genres.isNotEmpty()) {
-            must.add(QueryBuilders.terms { terms -> terms.field("genres").terms { it.value(fieldValues(genres)) } })
-        }
-
-        if (launchedAt != null) {
-            must.add(QueryBuilders.term { term -> term.field("launched_at").value(launchedAt!!.toLong()) })
-        }
-
-        if (rating.isNullOrBlank().not()) {
-            must.add(QueryBuilders.term { term -> term.field("rating").value(rating) })
-        }
-
-        if (terms.isNotBlank()) {
-            must.add(QueryBuilders.queryString { it.fields("title", "description").query("*$terms*") })
-        }
+        val aQueryBuilder = VideoQueryBuilder(
+            onlyPublished(),
+            containingCategories(categories),
+            containingCastMembers(castMembers),
+            containingGenres(genres),
+            launchedAtEquals(launchedAt),
+            ratingEquals(rating),
+            titleOrDescriptionContaining(terms)
+        ).build()
 
         val query = NativeQuery.builder()
-            .withQuery(QueryBuilders.bool { bool -> bool.must(must) })
+            .withQuery(aQueryBuilder)
             .withPageable(PageRequest.of(page, perPage, Sort.by(Direction.fromString(direction), buildSort(sort))))
             .build()
 
@@ -86,8 +72,6 @@ class VideoElasticsearchGateway(
     }
 
     override fun deleteById(id: String) = if (id.isBlank()) Unit else videoRepository.deleteById(id)
-
-    private fun fieldValues(ids: Set<String>): List<FieldValue> = ids.map { FieldValue.of(it) }
 
     private fun buildSort(sort: String) = if (TITLE_PROP.equals(sort, ignoreCase = true)) sort.plus(KEYWORD) else sort
 }
