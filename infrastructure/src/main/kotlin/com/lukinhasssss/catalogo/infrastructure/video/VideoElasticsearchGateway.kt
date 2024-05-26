15188bb2bbbcaf8a52e0433b7ddcc94dbaa0bc94
Lucas Monteiro <lukasmonteir0@hotmail.com>
2024-05-26 13:38:48 -0300

    feat: add implementation of method save of video elasticsearch repository

--- infrastructure/src/main/kotlin/com/lukinhasssss/catalogo/infrastructure/video/VideoElasticsearchGateway.kt
+++ infrastructure/src/main/kotlin/com/lukinhasssss/catalogo/infrastructure/video/VideoElasticsearchGateway.kt
@@ -4,15 +4,20 @@ import com.lukinhasssss.catalogo.domain.pagination.Pagination
 import com.lukinhasssss.catalogo.domain.video.Video
 import com.lukinhasssss.catalogo.domain.video.VideoGateway
 import com.lukinhasssss.catalogo.domain.video.VideoSearchQuery
+import com.lukinhasssss.catalogo.infrastructure.video.persistence.VideoDocument
+import com.lukinhasssss.catalogo.infrastructure.video.persistence.VideoRepository
 import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Component
 
 @Component
 @Profile("!development")
-class VideoElasticsearchGateway : VideoGateway {
+class VideoElasticsearchGateway(
+    private val videoRepository: VideoRepository
+) : VideoGateway {
 
     override fun save(video: Video): Video {
-        TODO("Not yet implemented")
+        videoRepository.save(VideoDocument.from(video))
+        return video
     }
 
     override fun findById(id: String): Video? {
