1367e6474b937b95392f986ccaff21dd0c8529c1
Lucas Monteiro <lukasmonteir0@hotmail.com>
2024-05-28 15:32:45 -0300

    feat: create video kafka listener

--- infrastructure/src/main/kotlin/com/lukinhasssss/catalogo/infrastructure/video/models/VideoDTO.kt
+++ infrastructure/src/main/kotlin/com/lukinhasssss/catalogo/infrastructure/video/models/VideoDTO.kt
@@ -1,5 +1,7 @@
 package com.lukinhasssss.catalogo.infrastructure.video.models
 
+import com.lukinhasssss.catalogo.domain.video.Video
+
 data class VideoDTO(
     val id: String,
     val title: String,
@@ -19,4 +21,27 @@ data class VideoDTO(
     val genresId: Set<String>,
     val createdAt: String,
     val updatedAt: String
-)
+) {
+    companion object {
+        fun from(video: Video) = VideoDTO(
+            id = video.id,
+            title = video.title,
+            description = video.description,
+            yearLaunched = video.launchedAt.value,
+            rating = video.rating.name,
+            duration = video.duration,
+            opened = video.opened,
+            published = video.published,
+            banner = video.banner,
+            thumbnail = video.thumbnail,
+            thumbnailHalf = video.thumbnailHalf,
+            trailer = video.trailer,
+            video = video.video,
+            categoriesId = video.categories,
+            castMembersId = video.castMembers,
+            genresId = video.genres,
+            createdAt = video.createdAt.toString(),
+            updatedAt = video.updatedAt.toString()
+        )
+    }
+}
